f51bd68ccb1a9b15ab8039c71c3b8d9e9dfa3457
Michael Delcambre <delcambrem@gmail.com>
2022-01-30 20:04:22 -0600

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,5 +9,10 @@ class StudentsController < ApplicationController
         students = Student.order(grade: :desc)
         render json: students
     end
-    
+
+    def highest_grade
+        student = Student.order(grade: :desc).first
+        render json: student
+    end
+
 end
